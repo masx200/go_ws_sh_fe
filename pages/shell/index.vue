cad17d1c1a17540d933ce9a9bf69cb052878f5e7
masx200 <masx200@qq.com>
2025-01-08 11:10:14 +0800

    Update index.vue

--- pages/shell/index.vue
+++ pages/shell/index.vue
@@ -13,10 +13,17 @@ const appopts = reactive({
     wsurl: "",
 });
 onMounted(() => {
-    const url = localStorage?.getItem("url");
+    const session = new URL(location.href).searchParams.get("session");
+    const url = new URL(location.href).searchParams.get("server");
     const token = localStorage?.getItem("token");
-    if (!token || !url) {
+    if (!token || !url || !session) {
         router.push("/");
+    } else {
+        appopts.wsprotocol = encodeURIComponent("token=" + token);
+        const url1 = new URL(url);
+        url1.protocol = url1.protocol.replace("http", "ws");
+        url1.pathname = session;
+        appopts.wsurl = url1.href;
     }
     // 这里可以添加初始化逻辑
 });
