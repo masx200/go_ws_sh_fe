6cba1bdd4e6aed7f8afa67502e52b6a9c8e401fc
masx200 <masx200@qq.com>
2025-03-26 22:15:49 +0800

    Update SessionAttributeEdit.vue

--- src/SessionAttributeEdit.vue
+++ src/SessionAttributeEdit.vue
@@ -14,24 +14,28 @@
                 <a-input v-model:value="sessionInfo.id" disabled />
             </a-form-item> -->
             <a-form-item
+            name="name"
                 label="会话名称"
                 :rules="[{ required: true, message: '请输入会话名称' }]"
             >
                 <a-input v-model:value="sessionInfo.name" />
             </a-form-item>
             <a-form-item
+                name="cmd"
                 label="命令"
                 :rules="[{ required: true, message: '请输入命令' }]"
             >
                 <a-input v-model:value="sessionInfo.cmd" />
             </a-form-item>
             <a-form-item
+                name="args"
                 label="参数"
                 :rules="[{ required: true, message: '请输入参数' }]"
             >
                 <a-input v-model:value="sessionInfo.args" />
             </a-form-item>
             <a-form-item
+                name="dir"
                 label="目录"
                 :rules="[{ required: true, message: '请输入目录' }]"
             >
@@ -47,6 +51,7 @@
 <script lang="ts">
 import { defineComponent, ref } from "vue";
 import { Form, Input, Button } from "ant-design-vue";
+import type { Rule } from "ant-design-vue/es/form/interface";
 
 export default defineComponent({
     components: {
@@ -56,6 +61,7 @@ export default defineComponent({
         "a-button": Button,
     },
     setup() {
+        const formRef = ref();
         const sessionInfo = ref({
             // id: "",
             name: "",
@@ -65,6 +71,8 @@ export default defineComponent({
         });
 
         const handleUpdateSessionAttributes = async () => {
+            await formRef.value
+                .validateFields()
             try {
                 // 调用 API 更新会话属性
                 // await api.updateSessionAttributes(sessionInfo.value);
@@ -74,7 +82,45 @@ export default defineComponent({
             }
         };
 
-        return {
+        return {formRef, rules: {
+                name: [
+                    {
+                        required: true,
+                        message: "请输入会话名称",
+                        trigger: "blur",
+                    },
+                    {
+                        min: 2,
+                        max: 50,
+                        message: "会话名称长度应在 2 到 50 个字符之间",
+                        trigger: "blur",
+                    },
+                ],
+                cmd: [
+                    { required: true, message: "请输入命令", trigger: "blur" },
+                    {
+                        pattern: /^[a-zA-Z0-9_\-/]+$/,
+                        message: "命令只能包含字母、数字、下划线、短横线或斜杠",
+                        trigger: "blur",
+                    },
+                ],
+                args: [
+                    { required: true, message: "请输入参数", trigger: "blur" },
+                    {
+                        max: 100,
+                        message: "参数长度不能超过 100 个字符",
+                        trigger: "blur",
+                    },
+                ],
+                dir: [
+                    { required: true, message: "请输入目录", trigger: "blur" },
+                    {
+                        pattern: /^([a-zA-Z]:)?(\/[a-zA-Z0-9_\- ]+)+$/,
+                        message: "请输入有效的目录路径",
+                        trigger: "blur",
+                    },
+                ],
+            } as Record<string, Rule[]>,
             sessionInfo,
             handleUpdateSessionAttributes,
         };
