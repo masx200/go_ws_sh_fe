4ed114e273cf9aaf49e90a6aff73f6e9266bcd8c
masx200 <masx200@qq.com>
2025-04-10 01:26:02 +0800

    form-rules

--- pages/login.vue
+++ pages/login.vue
@@ -228,7 +228,7 @@ const rules = reactive<FormRules<typeof loginForm>>({
     ],
     password: [
         { required: true, message: "请输入密码", trigger: "blur" },
-        { min: 4, max: 36, message: "长度在 4 到 36 个字符", trigger: "blur" },
+        { min: 4, max: 120, message: "长度在 4 到 120 个字符", trigger: "blur" },
     ],
 });
 
@@ -256,7 +256,10 @@ const tokenRules = reactive<FormRules<typeof tokenForm>>({
     //     { required: true, message: "请输入账号", trigger: "blur" },
     //     { min: 3, max: 30, message: "长度在 3 到 30 个字符", trigger: "blur" },
     // ],
-    token: [{ required: true, message: "请输入令牌", trigger: "blur" }],
+    token: [{ required: true, message: "请输入令牌", trigger: "blur" },
+  { min: 4, max: 120, message: "长度在 4 到 120 个字符", trigger: "blur" },
+
+],
     identifier: [{ required: true, message: "请输入标识", trigger: "blur" }],
 });
 
