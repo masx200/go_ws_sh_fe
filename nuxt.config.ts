fb7746e8d23315a359a89e9e3a8ed5792ab0ba89
masx200 <masx200@qq.com>
2025-09-27 19:25:49 +0100

    更新nuxt配置和依赖，添加dayjs-nuxt模块支持

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -63,7 +63,7 @@ export default defineNuxtConfig({
     },
     compatibilityDate: "2024-11-01",
     devtools: { enabled: true },
-    modules: ["@element-plus/nuxt", "@vite-pwa/nuxt",],
+    modules: ["@element-plus/nuxt", "@vite-pwa/nuxt",  'dayjs-nuxt'],
     vite: {
         optimizeDeps: {
             include: [], // 强制预打包
